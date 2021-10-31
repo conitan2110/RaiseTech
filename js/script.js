ff2e00b2ff7e7a41d5dbaa97804fedb19dbfd291
coni <aoi_ao726@yahoo.co.jp>
2021-10-31 23:56:47 +0900

    JSでドロップダウンメニュー途中

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$(".menu-btn").click(function () {
+// $(".menu-btn").click(function () {
+//     $(this).toggleClass('open');
+//     $(".global-nav").toggleClass('open');
+// });
+
+$(".menu-btn").on('click', function(){
     $(this).toggleClass('open');
+    $(".global-nav").toggleClass('open');
 });
+
+$(".has-menu>a").off('click');  // has-menuクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+    $(".has-menu>a").on('click', function(){  // has-menuクラスがついたaタグをクリックしたら
+      var parentElem =  $(this).parent();     // aタグから見た親要素の<li>を取得し
+    //   $(parentElem).children('ul').stop().slideToggle(100); // liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+    if ($(parentElem).children('ul').is(":hidden")) {
+        $(parentElem).children('ul').slideDown(100);
+    } else {
+        $(parentElem).children('ul').hide();
+    }
+    // $(parentElem).children('ul').slideDown(100);
+      return false;  // リンクの無効化
+    });
